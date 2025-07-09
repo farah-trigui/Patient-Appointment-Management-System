dbc40d69bfba5988a1c9417c6170e70279a0d959
Farah Trigui <farah.trigui2@insat.ucar.tn>
2025-07-09 11:16:04 +0100

    created repository for patients

---
+++ src/main/java/com/example/clinicTest/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package com.example.clinicTest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.clinicTest.entity.Patient;
+
+public interface PatientRepository extends JpaRepository<Patient, Long>{
+    boolean exitsByEmail(String email);
+    
+}
